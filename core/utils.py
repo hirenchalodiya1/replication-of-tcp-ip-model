d83a65114656a32f77ed4e389603e775cffa4d2d
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2019-11-06 21:40:55 +0530

    Load class function implemented

--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,14 @@
+from importlib import import_module
 VERBOSITY = 1
 
 
 def log(msg, verbosity=1):
     if verbosity >= VERBOSITY:
         print(msg)
+
+
+def load_class(path):
+    module, klass = path.rsplit('.', 1)
+    m = import_module(module)
+    cls = getattr(m, klass)
+    return cls
