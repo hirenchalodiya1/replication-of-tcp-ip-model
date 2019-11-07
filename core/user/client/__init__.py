2857c5a4d18257165d6c625cea40a1a9cd61707e
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2019-11-07 19:50:20 +0530

    Connect physical layer package and user package

--- core/user/client/__init__.py
+++ core/user/client/__init__.py
@@ -4,6 +4,7 @@ from core.utils import log
 from core.user.client.connection import create_connection
 from core.utils.str_byte_conversion import str2bytes
 from core.device.datalink.client import client_dll
+from core.device.physical.client import client_physical
 
 
 def run_client():
@@ -22,13 +23,18 @@ def run_client():
             log("Frames to be send: ", 2, end="")
             log(enc_frames, 2)
 
+            # Implement coding scheme to each frame
+            coded_frames = client_physical(enc_frames)
+            log("Encoded frames: ", 2, end="")
+            log(coded_frames, 2)
+
             # Send number of frames
             # num_of_frames = str(len(enc_frames))
             # encode_num_of_frames = client_dll(num_of_frames)[0]
             # sock.sendall(str2bytes(encode_num_of_frames))
 
             # Send the frames
-            for frame in enc_frames:
+            for frame in coded_frames:
                 sock.sendall(str2bytes(frame))
 
             # receive data from the server
