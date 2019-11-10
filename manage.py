fa1658f038c55ba8097777fb7fc4812c1af1b811
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2019-11-10 16:57:48 +0530

    Add ability to get server details from cmd

--- manage.py
+++ manage.py
@@ -1,12 +1,17 @@
 import sys
+import settings
 from core.user.server import run_server
 from core.user.client import run_client
 
 
 if __name__ == "__main__":
     args = sys.argv
-    if len(args) != 2:
+    if len(args) < 2:
         raise Exception("Please Provide valid number of argument")
+    if len(args) > 2:
+        host, port = args[2].split(":")
+        settings.SERVER_HOST = host
+        settings.SERVER_PORT = int(port)
     command = args[1]
     if command == "server":
         run_server()
